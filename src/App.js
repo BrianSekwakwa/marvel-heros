74cca3054f5aee06f3c81c8273127a08717db26f
Brian Sekwakwa <briansenpai14@gmail.com>
2019-07-27 14:54:27 +0200

    adding the header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from "./components/Header.js";
 import HeroForm from "./components/HeroForm";
 import HeroDescription from "./components/HeroDescription";
 import HeroComics from "./components/HeroComics";
@@ -7,12 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="hero-header">
-          <h1 className="hero-header__title">MARVEL HEROS</h1>
-          <p className="hero-header__lead">
-            Search For Your Favorite Marvel Super Hero
-          </p>
-        </div>
+        <Header />
         <HeroForm />
         {/* <HeroDescription />
         <HeroComics /> */}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div>some header is going to go here</div>
+    </div>
+  );
+}
+
+export default Header;
