ac8e6f4b3e3577fcd31d8553f33d810488ea1b42
Brian Sekwakwa <briansenpai14@gmail.com>
2019-08-08 11:51:28 +0200

    connected the application state from redux to the hero description component

--- src/components/HeroDescription.js
+++ src/components/HeroDescription.js
@@ -1,22 +1,35 @@
 import React from "react";
+import { connect } from "react-redux";
 
-function HeroDescription() {
-  return (
-    <div className="hero-bio">
-      <div className="hero-bio__image">
-        The heros description will go here once we fetch the data from the api
+function HeroDescription(props) {
+  if (props.heroData) {
+    
+    return (
+      <div className="hero-bio">
+        <div className="hero-bio__image">
+          The heros description will go here once we fetch the data from the api
+        </div>
+        <div className="hero-bio__content">
+          <span className="hero-bio__content__title">Hero Title</span>
+          <p className="hero-bio__content__description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+            voluptas rem tempora iusto laudantium enim aliquid reiciendis
+            dignissimos laboriosam, debitis culpa iure est exercitationem
+            fugiat, dolorem hic maiores, saepe autem.
+          </p>
+          <button className="hero-bio__content__button">Learn More</button>
+        </div>
       </div>
-      <div className="hero-bio__content">
-        <span>Hero Title</span>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
-          voluptas rem tempora iusto laudantium enim aliquid reiciendis
-          dignissimos laboriosam, debitis culpa iure est exercitationem fugiat,
-          dolorem hic maiores, saepe autem.
-        </p>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return <div />;
+  }
 }
 
-export default HeroDescription;
+const mapStateToProps = state => {
+  return {
+    heroData: state.heroData
+  };
+};
+
+export default connect(mapStateToProps)(HeroDescription);
