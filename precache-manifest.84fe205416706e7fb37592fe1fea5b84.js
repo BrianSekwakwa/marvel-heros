fd37c0fd01323e0be4306c0775e93d367bda3e44
Brian Sekwakwa <briansenpai14@gmail.com>
2019-08-11 17:43:19 +0200

    Updates

---
+++ precache-manifest.84fe205416706e7fb37592fe1fea5b84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beeb57a4990f413ddf5966cbf7cf84e7",
+    "url": "/marvel-heros/index.html"
+  },
+  {
+    "revision": "cb2c3bb81374d5d299d6",
+    "url": "/marvel-heros/static/css/main.e66cb1de.chunk.css"
+  },
+  {
+    "revision": "691022785bfcb5746eeb",
+    "url": "/marvel-heros/static/js/2.10eab176.chunk.js"
+  },
+  {
+    "revision": "cb2c3bb81374d5d299d6",
+    "url": "/marvel-heros/static/js/main.c2fbad5c.chunk.js"
+  },
+  {
+    "revision": "f8cca4a5f3465954a883",
+    "url": "/marvel-heros/static/js/runtime~main.4733523f.js"
+  },
+  {
+    "revision": "37cff92506be1c64434a720ae43cca14",
+    "url": "/marvel-heros/static/media/Image.37cff925.jpg"
+  },
+  {
+    "revision": "de1990109d31ffcfe6e814a2f56819d9",
+    "url": "/marvel-heros/static/media/loader.de199010.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb500bc81199caaab51b1ce6c9b98c1e.js"
+  "/marvel-heros/precache-manifest.84fe205416706e7fb37592fe1fea5b84.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marvel-heros/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
