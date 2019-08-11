abc5c8e5e3a2de5fcd695942558bb9804be27076
Brian Sekwakwa <briansenpai14@gmail.com>
2019-08-11 15:46:35 +0200

    Updates

---
+++ precache-manifest.cb500bc81199caaab51b1ce6c9b98c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b840b49782f3b3c13edd5cf33b3235ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b3b63cfae82157a4058",
+    "url": "/static/css/main.3fea461b.chunk.css"
+  },
+  {
+    "revision": "691022785bfcb5746eeb",
+    "url": "/static/js/2.10eab176.chunk.js"
+  },
+  {
+    "revision": "3b3b63cfae82157a4058",
+    "url": "/static/js/main.f720edbb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de1990109d31ffcfe6e814a2f56819d9",
+    "url": "/static/media/loader.de199010.gif"
+  }
+]);
